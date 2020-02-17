557ca9b189f9751cc14f3d7bee5bcfb22bdd4eb2
Daniel Kurtjak <daniel.kurtjak@gmail.com>
2020-02-17 22:53:51 +0100

    Mean, Median and Mode complete functional!

--- node.js
+++ node.js
@@ -1,76 +1,51 @@
 const {
-  countBy,
-  curry,
-  flip,
   compose,
   pipe,
   unapply,
   reduce,
   prop,
-  dec,
   identity,
   length,
-  zipWith,
-  gt,
   maxBy,
-  minBy,
-  lt,
-  takeLast,
-  nthArg,
+  equals,
   nth,
-  take,
+  dec,
   sum,
+  countBy,
+  divide,
+  converge,
+  flip,
+  modulo,
+  subtract,
   ifElse
 } = require("ramda");
 
-const add = (x, y) => x + y;
-const sub = (x, y) => x - y;
-const div = (x, y) => x / y;
-const mod = (x, y) => x % y;
-const equal = (x, y) => x === y;
-
-const modCurried = curry(flip(mod));
-const divCurried = curry(flip(div));
-const eqCurried = curry(equal);
-
 const println = tag => x => (console.log(`${tag}: ${JSON.stringify(x)}`), x);
 
-const div2 = divCurried(2);
-const mod2 = modCurried(2);
-const equal1 = eqCurried(1);
-
-// const array = " 10 8 6    4 12 11 4 1 11    "
-const array = " 64630 11735 14216 99233 14470 4978 73429 38120 51135 67060 "
+const array = " 10 8 6 9"
+  // const array = " 64630 11735 14216 99233 14470 4978 73429 38120 51135 67060 14470"
   .trim()
   .split(" ")
   .map(x => Number.parseInt(x))
   .filter(x => !Number.isNaN(x))
-  .sort(sub);
-
-const isOdd = pipe(mod2, equal1);
-
-const callTwice = fn => x => fn(x)(x);
-const functionalize = fn =>
-  curry((...fnArgs) => args => fn(...fnArgs.map(fn => fn(args))));
-
-const divFn = functionalize(div);
+  .sort(subtract);
 
 //MEAN
-const mean = divFn(sum, length);
-
+const mean = converge(divide, [sum, length]);
 const avg = unapply(mean);
-const avgF = functionalize(avg);
 
+const divFlipped = flip(divide);
+const div2 = divFlipped(2);
 const lengthDiv2 = compose(Math.floor, div2, length);
 
-//MEADIAN
+// MEADIAN
 const median = ifElse(
-  compose(isOdd, length),
-  callTwice(compose(prop, lengthDiv2)),
-  avgF(
-    callTwice(compose(prop, dec, lengthDiv2)),
-    callTwice(compose(prop, lengthDiv2))
-  )
+  compose(equals(1), modulo(2), length),
+  converge(prop, [lengthDiv2, identity]),
+  converge(avg, [
+    converge(prop, [compose(dec, lengthDiv2), identity]),
+    converge(prop, [lengthDiv2, identity])
+  ])
 );
 
 //MODE
