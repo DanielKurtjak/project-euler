a90fa516007946acf71ee85afbd724fab446134c
Daniel Kurtjak <daniel.kurtjak@gmail.com>
2020-02-17 23:36:20 +0100

    type weightedMean

--- src/weightedMedian.js
+++ src/weightedMedian.js
@@ -3,12 +3,12 @@ import R from "ramda";
 
 const { compose, zipWith, multiply, nthArg, sum, divide, converge } = R;
 
-const value = arrayFromString(" 10 40 30 50 3");
-const weigth = arrayFromString(" 1 2 3 4 5");
+const values = arrayFromString(" 10 40 30 50 3");
+const weigths = arrayFromString(" 1 2 3 4 5");
 
 export const weigthedMean = converge(divide, [
   compose(sum, zipWith(multiply)),
   compose(sum, nthArg(1))
 ]);
 
-console.log(`weigthedMean ${weigthedMean(value, weigth)}`);
+console.log(`weigthedMean ${weigthedMean(values, weigths)}`);
