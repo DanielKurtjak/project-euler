6d2988c38b2bca0b7254dabae3d9d60930f12380
Daniel Kurtjak <daniel.kurtjak@gmail.com>
2020-02-18 09:54:16 +0100

    Removing prints from weightedMean

--- src/weightedMedian.js
+++ src/weightedMedian.js
@@ -1,10 +1,6 @@
-import { arrayFromString } from "./helpers.js";
 import R from "ramda";
 
 const { compose, zipWith, multiply, nthArg, sum, divide, converge } = R;
-
-const values = arrayFromString(" 10 40 30 50 3");
-const weigths = arrayFromString(" 1 2 3 4 5");
 
 export const weigthedMean = converge(divide, [
   compose(sum, zipWith(multiply)),
