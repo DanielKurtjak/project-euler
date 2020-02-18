cc9296f05a51b23db6eacd1ef6798c31df3a7800
Daniel Kurtjak <daniel.kurtjak@gmail.com>
2020-02-18 15:30:09 +0100

    using __ in helper functions

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,6 @@
 import R from "ramda";
 
-const { compose, complement, flip, divide, mathMod, length, equals } = R;
+const { compose, complement, divide, mathMod, length, equals, __ } = R;
 
 export const println = tag => x => (
   console.log(`${tag}: ${JSON.stringify(x)}`), x
@@ -13,11 +13,8 @@ export const arrayFromString = string =>
     .map(x => Number.parseInt(x))
     .filter(x => !Number.isNaN(x));
 
-const divFlipped = flip(divide);
-const modFlipped = flip(mathMod);
-
-export const div2 = divFlipped(2);
-export const mod2 = modFlipped(2);
+export const div2 = divide(__, 2);
+export const mod2 = mathMod(__, 2);
 export const lengthDiv2 = compose(Math.floor, div2, length);
 export const isOdd = compose(equals(1), mod2);
 export const isEven = complement(isOdd);
--- src/node.js
+++ src/node.js
@@ -1,5 +1,34 @@
 import { arrayFromString } from "./helpers.js";
 import { interquartileRange } from "./interquartileRange.js";
+import R from "ramda";
+
+const {
+  compose,
+  pipe,
+  subtract,
+  apply,
+  unapply,
+  reduce,
+  prop,
+  identity,
+  sort,
+  length,
+  maxBy,
+  nth,
+  dec,
+  sum,
+  countBy,
+  zipWith,
+  divide,
+  repeat,
+  converge,
+  reverse,
+  flatten,
+  ifElse,
+  filter,
+  map,
+  toUpper
+} = R;
 
 const values = arrayFromString(
   "6 12 8 10 20"
@@ -11,3 +40,28 @@ const frequences = arrayFromString(
 );
 
 console.log(`interquartileRange ${interquartileRange(values, frequences)}`);
+
+// export const interquartileRange = compose(
+//   converge(subtract, [quartiles3, quartiles1]),
+//   flatten,
+//   zipWith(repeat)
+// );
+
+// const users = [
+//   { name: "Daniel", age: 35 },
+//   { name: "Cvija", age: 36 },
+//   { name: "Dalibor", age: 38 }
+// ];
+
+// // const getProperty = (propName, obj) => obj[propName];
+// const getProperty = propName => obj => obj[propName];
+
+// const fn = (a1, a2, a3) => a1 + a2 + a3;
+// const fnCurried = a1 => a2 => a3 => a1 + a2 + a3;
+
+// fn(1, 3, 5);
+// fnCurried(1)(3)(5);
+
+// const getName = getProperty("name");
+// const upperName = compose(toUpper, getName);
+// console.log({ names: map(upperName, users) });
